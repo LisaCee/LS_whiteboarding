c22bbf4a82717420e2dd9c9f2ffd87e466eebf1c
LisaCee <lisaceedesign@gmail.com>
2019-04-30 18:56:58 -0700

    counting votes works alpha for ties

---
+++ ClimbingStairs/my_solution.js
@@ -0,0 +1,19 @@
+// const climbStairs = n => {
+//     let steps = 0;
+
+//     if (n < 0) return 0;
+//     if (n == 0) return 1;
+    
+//     else {
+//         return climbStairs(n - 1) + climbStairs(n - 2) + climbStairs(n - 3);
+//     }
+// }
+
+const climbStairs = n => {
+    let cache = {};
+
+    
+    return cache;
+}
+
+console.log(climbStairs(3)) //4
--- CountingVotes/my_solution.js
+++ CountingVotes/my_solution.js
@@ -1,4 +1,5 @@
-// Counting Votes
+// ****************************************************************************
+// Count Votes
 // You've been hired to write the software to count the votes for a local election. Write a function countVotes that receives an array of (unique) names, each one representing a vote for that person. Your function should return the name of the winner of the election. In the case of a tie, the person whose name comes last alphabetically wins the election (a dumb rule to be sure, but the votes don't need to know).
 
 // Example:
@@ -10,28 +11,30 @@
 
 const votes = ['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael'];
 
-const countVotes = (arr) => {
-  let counter = {};
-  let maxVotes = 0;
-  let tieBreaker = []
-  for (let i = 0; i < arr.length; i++) {
+const countVotes = arr => {
+  let cache = {};
+  let tieBreaker = [];
+  let maxVotes;
 
-    if (counter.hasOwnProperty(arr[i])) {
-      counter[arr[i]]++
+  for (let i = 0; i < votes.length; i++) {
+    if (!cache[votes[i]]) {
+      cache[votes[i]] = 1
     } else {
-      counter[arr[i]] = 1;
+      cache[votes[i]] ++;
     }
   }
-  for (name in counter) {
-    if (counter[name] > maxVotes) {
-      maxVotes = counter[name];
- 
+  let array = Object.entries(cache);
+  array.sort((a, b) => a[1] - b[1])
+  for (let i = 1; i < array.length; i++) {
+    maxVotes = array[i][1]
+    if(array[i][1] >= maxVotes) {
+      maxVotes = array[i][1]
+      tieBreaker.push(array[i][0])
     }
-  } 
-  // console.log(counter);
-  let solution = Object.entries(counter);
-  return solution.sort((a, b) => a[1] - b[1])[solution.length - 1][0]
-
+  }
+  let solution = tieBreaker.sort();
+  return solution[solution.length - 1]
 }
-countVotes(votes); 
+
+countVotes(votes)
 
